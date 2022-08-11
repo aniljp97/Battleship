9bafc761c9bf1f23f95ab745ebe61d2953f6bf74
aniljp97 <aniljp97@users.noreply.github.com>
2022-08-11 02:08:15 -0500

    Add files via upload

---
+++ Battleship/Battleship/Ship.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battleship
+{
+    public class Ship
+    {
+        public int length;
+        public SquareState squareState;
+
+        public Ship(int length, SquareState squareState)
+        {
+            this.length = length;
+            this.squareState = squareState;
+        }
+    }
+}
